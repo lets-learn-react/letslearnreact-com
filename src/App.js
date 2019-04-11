ec9c1f57c7bc6d89194cd0cc7529cfc29423e2f7
mykola2306 <mykolag21@gmail.com>
2019-04-12 01:56:17 +0300

    Add splash page

--- src/App.js
+++ src/App.js
@@ -1,14 +1,34 @@
 import React, { Component } from 'react'
 // import Layout from './components/Layout'
 import Album from './components/ManyComponentsInOne'
+import SplashPage from './components/SplashPage'
+
+const Pages = {
+  splashPage: 'Splash Page',
+  jobBoard: 'Job Board'
+}
 
 class App extends Component {
-  render () {
-    return (
-      <div>
-        <Album />
-      </div>
-    )
+  constructor(){
+    super()
+    this.state = {
+      component: 'Splash Page'
+    }
+    this.goToComponent = this.goToComponent.bind(this)
+  }
+  goToComponent(component){
+    this.setState({component})
+    document.body.scrollIntoView();
+  }
+  render () {    
+    switch (this.state.component) {
+      case 'Splash Page':
+        return <SplashPage goTo={this.goToComponent} pages={Pages} />        
+      case 'Job Board':
+        return <Album />        
+      default:
+        return (<h1>Page Not Found</h1>)
+    }
   }
 }
 
